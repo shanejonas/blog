c6d3a2ebed5fb58c26a45a9bb9bdbd4470bd59bd
Shane Jonas <shane@lawlmedia.com>
2010-12-30 12:22:12 -0500

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+before_filter :authenticate_user!, :only => [:new, :edit, :update, :destroy]
+
+
+def index
+  @posts = Post.all
+end
+
+def show
+  @post = Post.find(params[:id])
+end
+
+def new
+  if request.post?
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to "/posts/#{@post.id}/"
+    end
+    else
+      @post = Post.new
+    end
+end
+
+
+def edit
+    @post = Post.find(params[:id])
+end
+
+def update
+ @post = Post.find(params[:id]) 
+  respond_to do |format|
+      if @post.update_attributes(params[:post])
+        format.html { redirect_to(@post, :notice => 'Post updated') }
+ 
+      else
+        format.html { render :action => "edit" }
+
+      end
+    end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  respond_to do |format|
+      format.html { redirect_to(posts_url) }
+  end
+
+end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = 'e92387673bc0e536804ba5ffab18e50723a345af0a07e6c7c967ec1a37bc8f74f9f7125c3d540eec34b204254f3451a51291de39f4697901090a61248d0b7b37'
